e54e7a002fd50b68efae776242a2474d94f1315b
MS-Shahzaib-code <160013511+MS-Shahzaib-code@users.noreply.github.com>
2024-10-17 01:46:01 +0500

    add all file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,87 @@
-import Image from "next/image";
+ import Image from "next/image";
+import guitar from "./IMAGES/eddies-guitar-3-gary-bodnar.jpg"
+import piano from "./IMAGES/th.jpeg"
+import Percussion from "./IMAGES/th (2).jpeg"
+import Woodwind from "./IMAGES/th (3).jpeg"
+import Brass from "./IMAGES/th (1).jpeg"
+import Drum from "./IMAGES/th (4).jpeg"
 
 export default function Home() {
+  type Data = {
+    title: string;
+    description: string;
+    image : any;
+  }
+  let data: Data[] = [
+    {
+      title: "Guitars",
+      description:
+        " Acoustic Guitars :These produce sound acoustically through the vibration of strings over a hollow chamber. ",
+       image : guitar ,
+    },
+    {
+      title: "piano",
+      description:
+        "The modern piano was invented around 1700 by Bartolomeo Cristofori in Italy.",
+        image : piano ,
+    },
+    {
+      title: "Percussion",
+      description:
+        "Pitched Percussion : Instruments that produce specific notes, like xylophones, marimbas, and timpani.",
+        image : Percussion ,
+    },
+    {
+      title: "Woodwind",
+      description:
+        "Clarinet : Uses a single reed to produce sound. It has a rich, warm tone and is versatile across genres.",
+        image : Woodwind ,
+    },
+    {
+      title: "Brass",
+      description:
+        "Trumpet: The highest-pitched brass instrument, its versatile and used in various music genres, from classical to jazz.",
+        image : Brass ,
+    },
+    {
+      title: "Drum",
+      description:
+        "Snare Drum: Known for its sharp, staccato sound. A central part of drum kits.",
+        image : Drum ,
+    },
+  ];
+  {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+    <div className="min-h-screen flex flex-col justify-between bg-gray-950">
+      <header className="bg-gray-800 text-white py-4 w-full sm:w-[1080px] rounded-full pl-4 sm:pl-12 mt-8 mx-auto">
+        <nav className="flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0">
+          <h1 className="text-3xl font-bold">My Blog</h1>
+          <ul className="flex space-x-4 font-bold text-lg">
+            <li><a href="#" className="hover:underline">Home</a></li>
+            <li><a href="#" className="hover:underline">About</a></li>
+            <li><a href="#" className="hover:underline">Contact Us</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main className="flex flex-wrap justify-evenly w-full sm:w-[1320px] mx-auto mt-8 space-y-6">
+        {data.map((item, index) => (
+          <div key={index} className="flex-grow max-w-sm p-6 bg-white shadow-2xl rounded-lg border border-gray-200 bg-stone-900 m-4">
+            <Image className="w-full h-48 rounded-t-lg object-cover" src={item.image} alt="this is my guitar blog" />
+            <div className="mt-4">
+              <h1 className="text-2xl font-semibold text-gray-800">{item.title}</h1>
+              <p className="mt-2 text-gray-600">{item.description}</p>
+            </div>
+          </div>
+        ))}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="bg-gray-800 text-white py-4 mt-4">
+        <p className="text-center">© 2024 My Blog. All rights reserved.</p>
       </footer>
     </div>
   );
 }
+}
+ 
+
+
+ 
